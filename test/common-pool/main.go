7d3535814431fb6c5c7312154e0a956fd5f9ed54
panlin <272556213@qq.com>
2017-09-20 06:11:27 -0400

    common pool

--- test/common-pool/main.go
+++ test/common-pool/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/jolestar/go-commons-pool"
+	"log"
+	"math"
 	"reflect"
+	"time"
 )
 
 type MyPoolObject struct {
@@ -14,40 +17,78 @@ type MyObjectFactory struct {
 }
 
 func (f *MyObjectFactory) MakeObject() (*pool.PooledObject, error) {
-	fmt.Println("MakeObject")
+	log.Println("MakeObject")
 	return pool.NewPooledObject(&MyPoolObject{Name: "panlin"}), nil
 }
 
 func (f *MyObjectFactory) DestroyObject(object *pool.PooledObject) error {
 	//do destroy
-	fmt.Println("DestroyObject")
+	log.Println("DestroyObject")
 	return nil
 }
 
 func (f *MyObjectFactory) ValidateObject(object *pool.PooledObject) bool {
 	//do validate
-	fmt.Println("ValidateObject")
-	return true
+	log.Println("ValidateObject")
+	return false
 }
 
 func (f *MyObjectFactory) ActivateObject(object *pool.PooledObject) error {
 	//do activate
-	fmt.Println("ActivateObject")
+	log.Println("ActivateObject")
 	return nil
 }
 
 func (f *MyObjectFactory) PassivateObject(object *pool.PooledObject) error {
 	//do passivate
-	fmt.Println("PassivateObject")
+	log.Println("PassivateObject")
 	return nil
 }
 
+func PoolInfo(pool *pool.ObjectPool) {
+	fmt.Printf(" GetNumIdle: %d\n", pool.GetNumIdle())
+	fmt.Printf(" GetNumActive: %d\n", pool.GetNumActive())
+	fmt.Printf(" GetDestroyedCount: %d\n", pool.GetDestroyedCount())
+	fmt.Printf(" GetDestroyedByBorrowValidationCount: %d\n", pool.GetDestroyedByBorrowValidationCount())
+}
+
+var config *pool.ObjectPoolConfig = &pool.ObjectPoolConfig{
+	MaxTotal:                       10,
+	MaxIdle:                        10,
+	MinIdle:                        0,
+	Lifo:                           true,
+	MaxWaitMillis:                  int64(-1),
+	MinEvictableIdleTimeMillis:     int64(1000 * 60 * 30),
+	SoftMinEvictableIdleTimeMillis: int64(math.MaxInt64),
+	NumTestsPerEvictionRun:         3,
+	TestOnCreate:                   false,
+	TestOnBorrow:                   false,
+	TestOnReturn:                   false,
+	TestWhileIdle:                  true,
+	TimeBetweenEvictionRunsMillis:  int64(1), //int64(-1)
+	BlockWhenExhausted:             true,
+	EvictionPolicyName:             "github.com/jolestar/go-commons-pool/DefaultEvictionPolicy"}
+
 func main() {
-	pool := pool.NewObjectPoolWithDefaultConfig(new(MyObjectFactory))
-	obj, _ := pool.BorrowObject()
+	opool := pool.NewObjectPool(new(MyObjectFactory), config)
+
+	for i := 0; i < 10; i++ {
+		err := opool.AddObject()
+		if err != nil {
+			log.Printf("addobject failed: %s\n", err)
+		}
+		fmt.Printf("i: %d\n", i)
+	}
+
+	obj, _ := opool.BorrowObject()
 
 	fmt.Println(reflect.TypeOf(obj))
 	fmt.Println(obj.(*MyPoolObject).Name)
 
-	pool.ReturnObject(obj)
+	for i := 0; i < 5; i++ {
+		PoolInfo(opool)
+		time.Sleep(1)
+	}
+
+	opool.ReturnObject(obj)
 }
