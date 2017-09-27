8a89f7965baa76986545e3726acb1bb262d6fd05
panlin <272556213@qq.com>
2017-09-27 00:52:03 -0400

    mgo

---
+++ test/conn-db/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Person struct {
+	Name string `json:"name", bson:"name"`
+	Age  int    `json:"age", bson:"age"`
+}
+
+func main() {
+	//mongodb://127.0.0.1:27017 : local db
+	session, err := mgo.Dial("mongodb://127.0.0.1:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	//database: panlin, collection: col
+	col := session.DB("panlin").C("col")
+
+	// Optional. Switch the session to a monotonic behavior.
+	//session.SetMode(mgo.Monotonic, true)
+
+	//insert
+	err = col.Insert(&Person{Name: "liyuan", Age: 26})
+	if err != nil {
+		fmt.Println("[ERROR] insert liyuan is failed!")
+		return
+	}
+
+	//find
+	result := Person{}
+	err = col.Find(bson.M{"name": "liyuan"}).One(&result)
+	if err != nil {
+		fmt.Println("[ERROR] name: panlin is not exist!")
+		return
+	}
+
+	fmt.Printf("liyuan age: %d\n", result.Age)
+
+	//remove
+	info, err := col.RemoveAll(bson.M{"name": "liyuan"})
+	if err != nil {
+		fmt.Println("[ERROR] remove all failed!")
+		return
+	}
+
+	fmt.Printf("info: %#v\n", info)
+
+	//update
+	info, err = col.UpdateAll(bson.M{"name": "panlin"}, &Person{Name: "panxinwei", Age: 8})
+	if err != nil {
+		fmt.Println("[ERROR] update all failed!")
+		return
+	}
+
+	fmt.Printf("info: %#v\n", info)
+}
