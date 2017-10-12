9dba9a80465bc7a84f5c6f9d9365611b07fa4e11
panlin <272556213@qq.com>
2017-10-12 02:39:38 -0400

    runtime base

--- runtime-pkg/base/main.go
+++ runtime-pkg/base/main.go
@@ -2,34 +2,98 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 	"sync"
 )
 
 var wg sync.WaitGroup
 
+func PrintGoInfo() {
+	fmt.Println("go version: ", runtime.Version())
+}
+
+func PrintCpuInfo() {
+	//
+	fmt.Printf("CPU NUM: %d\n", runtime.NumCPU())
+	//
+	fmt.Printf("GOROOT: %s\n", runtime.GOROOT())
+	//
+	fmt.Printf("GOOS: %s\n", runtime.GOOS)
+}
+
+func PrintFunInfo(layer int) bool {
+	// get call pc
+	pc, _, _, ok := runtime.Caller(layer)
+	if !ok {
+		fmt.Println("caller is failed!")
+		return false
+	}
+
+	// create type Func
+	funinfo := runtime.FuncForPC(pc)
+	file, line := funinfo.FileLine(funinfo.Entry())
+	fmt.Printf("file : %s\nline : %d\n", file, line)
+	fmt.Printf("fun name : %s\n", funinfo.Name())
+
+	return true
+}
+
+func SetEnv() {
+	// open GC debug
+	os.Setenv("GOGCTRACE", "1")
+}
+
+func UnSetEnv() {
+	os.Unsetenv("GOGCTRACE")
+}
+
 func display(num int) {
+	fmt.Println("goroutine num: ", runtime.NumGoroutine())
 
 	for i := 1; i < num; i++ {
 		fmt.Println(i)
+		//runtime.Gosched()
 	}
 
 	wg.Done()
+
+	runtime.Goexit()
+
+	fmt.Println("Goruntine exit")
+}
+
+// system func
+func init() {
+	fmt.Println("init start : ------------------------------------------------------------------------------------------")
+	//
+	SetEnv()
+
+	//
+	PrintGoInfo()
+
+	// print cpu info
+	PrintCpuInfo()
+
+	fmt.Println("init start : ------------------------------------------------------------------------------------------")
+}
+
+func clean() {
+	//
+	UnSetEnv()
 }
 
 func main() {
-	//
-	cpunum := runtime.NumCPU()
-	fmt.Printf("cpu-num: %d\n", cpunum)
+	//print fun info
+	if ok := PrintFunInfo(1); !ok {
+		return
+	}
 
+	cpunum := runtime.NumCPU()
 	wg.Add(cpunum)
 
-	//
-	goroot := runtime.GOROOT()
-	fmt.Printf("GOROOT: %s\n", goroot)
-
-	//
-	fmt.Printf("GOOS: %s\n", runtime.GOOS)
+	//run GC
+	runtime.GC()
 
 	// set max cpu
 	runtime.GOMAXPROCS(cpunum)
